19348bb50b02cb193203eb38b6734cf3b1269f96
GaetanGrd <40648115+GaetanGrd@users.noreply.github.com>
2026-08-25 09:54:57 +0200

    feat: prototype Discord settings concept 4

---
+++ src/Aeziol.App/DiscordSettingsV4/DiscordSettingsV4Concept4.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace Aeziol.App.DiscordSettingsV4;
+
+public partial class DiscordSettingsV4Concept4 : Window
+{
+    public DiscordSettingsV4Concept4()
+    {
+        InitializeComponent();
+    }
+}
